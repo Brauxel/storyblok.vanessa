de1f3e8f86b9209c85251d71aee068addb805c54
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2020-04-12 15:55:38 +1000

    config updated to match the suggestion on TS website

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,29 @@ module.exports = {
     extensions: [".ts", ".tsx", ".js"],
   },
   module: {
-    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+    rules: [
+      { test: /\.ts(x?)$/, loader: "ts-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js(x?)$/,
+        loader: "source-map-loader",
+      },
+    ],
   },
   devtool: "inline-source-map",
   devServer: {
     hot: true,
     https: true,
     open: true,
+  },
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
   },
   plugins: [
     new HtmlWebpackPlugin({
